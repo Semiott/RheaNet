63dd5aa5c6bcf79d67269a4f5ec6c728323be935
Gokul Alex <gokulsan@users.noreply.github.com>
2021-04-10 19:21:00 +0530

    Create GoodsNFT.sol

---
+++ DisputeChain/contracts/GoodsNFT.sol
@@ -0,0 +1,32 @@
+pragma solidity 0.6.6;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+
+contract GoodsNFT is ERC721, Ownable {
+
+  using Counters for Counters.Counter;
+  Counters.Counter private _tokenIds;
+
+  constructor(string memory name, string memory symbol) 
+    public 
+    ERC721(name, symbol)   
+  {
+    _setBaseURI("https://ipfs.io/ipfs/");
+  }
+
+  function mintItem(address to, string memory tokenURI)
+      public
+      returns (uint256)
+  {
+      _tokenIds.increment();
+
+      uint256 id = _tokenIds.current();
+      _mint(to, id);
+      _setTokenURI(id, tokenURI);
+
+      return id;
+  }
+}
