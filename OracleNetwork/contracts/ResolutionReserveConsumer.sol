16eda13d0e6b48728b912448644b87d02a9928a0
Gokul Alex <gokulsan@users.noreply.github.com>
2021-04-12 02:39:38 +0530

    Update ResolutionReserveConsumer.sol

--- OracleNetwork/contracts/ResolutionReserveConsumer.sol
+++ OracleNetwork/contracts/ResolutionReserveConsumer.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@nomiclabs/buidler/console.sol";
-
 import "@chainlink/contracts/src/v0.6/ChainlinkClient.sol";
 
 contract ResolutionReserverConsumer is ChainlinkClient {
